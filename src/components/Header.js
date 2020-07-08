c2bc6948bb84da21998e54e43866139691b46922
faisalrahman36@hotmail.com <faisalrahman36@hotmail.com>
2020-07-09 04:36:21 +0500

    expense tracker

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,6 +7,7 @@ export const Header = () => {
       Expense Tracker 
     </h1>
     <br/> <p><i>by Syed Faisal ur Rahman</i></p> 
+    <i>Help taken from: <a href="https://github.com/adil-innovation-lab/react-expense-tracker">Adil Altaf's Example</a> </i>
     </div>
   )
 }
